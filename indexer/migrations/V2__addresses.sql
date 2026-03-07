a523a5aefb69abdeb3889ef146be1c73fae346b4
pshenmic <pshenmic@gmail.com>
2026-03-08 06:05:40 +0700

    Fix migration

--- indexer/migrations/V2__addresses.sql
+++ indexer/migrations/V2__addresses.sql
@@ -1,8 +1,8 @@
 CREATE TABLE addresses (
     address varchar(35) PRIMARY KEY ,
-    first_seen_tx   CHAR(64) references transactions(txid),
+    first_seen_tx   CHAR(64) references transactions(hash),
     first_seen_block INT references blocks(height),
-    last_seen_tx CHAR(64) references transactions(txid),
+    last_seen_tx CHAR(64) references transactions(hash),
     last_seen_block INT references blocks(height),
     CONSTRAINT address_unique UNIQUE(address)
 );
