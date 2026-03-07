a0f1b6cbeaa7dc34fdcc03dc8e5698964e59513b
pshenmic <pshenmic@gmail.com>
2026-03-08 06:10:38 +0700

    Add transaction history chart

--- api/src/controllers/TransactionsController.ts
+++ api/src/controllers/TransactionsController.ts
@@ -29,6 +29,11 @@ export default class TransactionsController {
     response.send(transaction);
   };
 
+  getTransactionHistory = async (request: FastifyRequest, response: FastifyReply): Promise<void> => {
+    const history = await this.transactionsDAO.getTransactionHistory();
+    response.send(history);
+  };
+
   getTransactionsByBlockHeight = async (request: FastifyRequest<{ Querystring: { page?: number; limit?: number; order?: string }; Params: { height?: number } }>, response: FastifyReply): Promise<void> => {
     const { page = 1, limit = 10, order = 'asc' } = request.query;
     const { height = 1 } = request.params;
