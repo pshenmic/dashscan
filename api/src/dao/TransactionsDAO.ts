983015fb3653fe6ac92d0aab73d51bf45b2a3e91
owl352 <kokosinca123@gmail.com>
2026-04-13 18:14:57 +0300

    update dao for input amount

--- api/src/dao/TransactionsDAO.ts
+++ api/src/dao/TransactionsDAO.ts
@@ -39,50 +39,51 @@ export default class TransactionsDAO {
       .limit(limit)
       .offset(fromRank)
 
-    const outputsSubquery = this.knex('tx_outputs')
-      .whereRaw('tx_outputs.tx_id = subquery.id')
-      .select(this.knex.raw('json_agg(tx_outputs.*) as outputs'));
+    const outputsCTE = this.knex('tx_outputs')
+      .select('tx_id')
+      .select(this.knex.raw('json_agg(tx_outputs.*) as outputs'))
+      .whereIn('tx_id', this.knex('subquery').select('id'))
+      .groupBy('tx_id');
 
-    const inputsSubquery = this.knex('tx_inputs')
+    const inputsCTE = this.knex('tx_inputs')
       .leftJoin('addresses', 'addresses.id', 'tx_inputs.address_id')
-      .whereRaw('tx_inputs.tx_id = subquery.id')
+      .leftJoin('tx_outputs', function () {
+        this.on('tx_outputs.tx_id', '=', 'tx_inputs.prev_tx_id')
+          .andOn('tx_outputs.vout_index', '=', 'tx_inputs.prev_vout_index');
+      })
+      .whereIn('tx_inputs.tx_id', this.knex('subquery').select('id'))
+      .select('tx_inputs.tx_id')
       .select(this.knex.raw(`
         json_agg(
           json_build_object(
             'prev_tx_hash', tx_inputs.prev_tx_hash,
             'prev_vout_index', tx_inputs.prev_vout_index,
             'address', addresses.address,
-            'address_id', tx_inputs.address_id
+            'amount', tx_outputs.value::text
           )
         ) as inputs
-      `));
+      `))
+      .groupBy('tx_inputs.tx_id');
 
     const rows = await this.knex
       .with('subquery', subquery)
       .with('total_count', countSubquery)
+      .with('agg_outputs', outputsCTE)
+      .with('agg_inputs', inputsCTE)
       .select(this.knex.raw('max_height - block_height + 1 AS confirmations'))
       .select(this.knex('total_count').as('total_count'))
       .select(
         'subquery.hash', 'type', 'block_height',
         'blocks.timestamp as timestamp', 'chain_locked',
         'blocks.hash as block_hash', 'instant_lock',
-        'inputs.inputs', 'outputs.outputs'
+        'agg_inputs.inputs', 'agg_outputs.outputs'
       )
-      .leftJoin(this.knex.raw(`
-        LATERAL (
-          ${outputsSubquery.toQuery()}
-        ) outputs ON true
-      `))
-      .leftJoin(
-        this.knex.raw(`
-          LATERAL (
-            ${inputsSubquery.toQuery()}
-          ) inputs ON true
-        `)
-      )
+      .leftJoin('agg_outputs', 'agg_outputs.tx_id', 'subquery.id')
+      .leftJoin('agg_inputs', 'agg_inputs.tx_id', 'subquery.id')
       .join(blockMaxHeightSubquery, this.knex.raw('true'))
       .leftJoin('blocks', 'blocks.height', 'block_height')
-      .from('subquery')
+      .from('subquery');
+
 
     const [row] = rows;
 
@@ -94,54 +95,61 @@ export default class TransactionsDAO {
       .select(this.knex.raw('MAX(height) as max_height'))
       .as('height_subquery')
 
-    const outputsSubquery = this.knex('tx_outputs')
-      .whereRaw('tx_outputs.tx_id = transactions.id')
-      .select(this.knex.raw('json_agg(tx_outputs.*) as outputs'));
+    const outputsCTE = this.knex('tx_outputs')
+      .select('tx_id')
+      .select(this.knex.raw('json_agg(tx_outputs.*) as outputs'))
+      .whereIn('tx_id', this.knex('subquery').select('id'))
+      .groupBy('tx_id');
 
-    const inputsSubquery = this.knex('tx_inputs')
+    const inputsCTE = this.knex('tx_inputs')
       .leftJoin('addresses', 'addresses.id', 'tx_inputs.address_id')
-      .whereRaw('tx_inputs.tx_id = transactions.id')
+      .leftJoin('tx_outputs', function () {
+        this.on('tx_outputs.tx_id', '=', 'tx_inputs.prev_tx_id')
+          .andOn('tx_outputs.vout_index', '=', 'tx_inputs.prev_vout_index');
+      })
+      .whereIn('tx_inputs.tx_id', this.knex('subquery').select('id'))
+      .select('tx_inputs.tx_id')
       .select(this.knex.raw(`
         json_agg(
           json_build_object(
             'prev_tx_hash', tx_inputs.prev_tx_hash,
             'prev_vout_index', tx_inputs.prev_vout_index,
             'address', addresses.address,
-            'address_id', tx_inputs.address_id
+            'amount', tx_outputs.value::text
           )
         ) as inputs
-      `));
+      `))
+      .groupBy('tx_inputs.tx_id');
 
-    const row = await this.knex('transactions')
+    const subquery = this.knex('transactions')
+      .where('transactions.hash', hash.trim())
+      .limit(1)
+
+    const row = await this.knex
+      .with('subquery', subquery)
+      .with('agg_outputs', outputsCTE)
+      .with('agg_inputs', inputsCTE)
       .select(
-        'transactions.hash',
-        'transactions.type',
-        'transactions.version',
-        'transactions.size',
-        'transactions.locktime',
-        'transactions.is_coinbase',
-        'transactions.block_height',
-        'transactions.instant_lock',
-        'transactions.chain_locked',
+        'subquery.hash',
+        'subquery.type',
+        'subquery.version',
+        'subquery.size',
+        'subquery.locktime',
+        'subquery.is_coinbase',
+        'subquery.block_height',
+        'subquery.instant_lock',
+        'subquery.chain_locked',
         'blocks.hash as block_hash',
         'blocks.timestamp as timestamp',
       )
       .select(this.knex.raw('max_height - block_height + 1 AS confirmations'))
-      .select('inputs.inputs', 'outputs.outputs')
-      .leftJoin(this.knex.raw(`
-        LATERAL (
-          ${outputsSubquery.toQuery()}
-        ) outputs ON true
-      `))
-      .leftJoin(this.knex.raw(`
-        LATERAL (
-          ${inputsSubquery.toQuery()}
-        ) inputs ON true
-      `))
+      .select('agg_inputs.inputs', 'agg_outputs.outputs')
+      .leftJoin('agg_outputs', 'agg_outputs.tx_id', 'subquery.id')
+      .leftJoin('agg_inputs', 'agg_inputs.tx_id', 'subquery.id')
       .join(blockMaxHeightSubquery, this.knex.raw('true'))
-      .leftJoin('blocks', 'blocks.height', 'transactions.block_height')
-      .where('transactions.hash', hash.trim())
-      .first();
+      .leftJoin('blocks', 'blocks.height', 'subquery.block_height')
+      .first()
+      .from('subquery');
 
     if (!row) return null;
 
@@ -184,26 +192,36 @@ export default class TransactionsDAO {
       .limit(limit)
       .offset(fromRank)
 
-    const outputsSubquery = this.knex('tx_outputs')
-      .whereRaw('tx_outputs.tx_id = subquery.id')
-      .select(this.knex.raw('json_agg(tx_outputs.*) as outputs'));
+    const outputsCTE = this.knex('tx_outputs')
+      .select('tx_id')
+      .select(this.knex.raw('json_agg(tx_outputs.*) as outputs'))
+      .whereIn('tx_id', this.knex('subquery').select('id'))
+      .groupBy('tx_id');
 
-    const inputsSubquery = this.knex('tx_inputs')
+    const inputsCTE = this.knex('tx_inputs')
       .leftJoin('addresses', 'addresses.id', 'tx_inputs.address_id')
-      .whereRaw('tx_inputs.tx_id = subquery.id')
+      .leftJoin('tx_outputs', function () {
+        this.on('tx_outputs.tx_id', '=', 'tx_inputs.prev_tx_id')
+          .andOn('tx_outputs.vout_index', '=', 'tx_inputs.prev_vout_index');
+      })
+      .whereIn('tx_inputs.tx_id', this.knex('subquery').select('id'))
+      .select('tx_inputs.tx_id')
       .select(this.knex.raw(`
         json_agg(
           json_build_object(
             'prev_tx_hash', tx_inputs.prev_tx_hash,
             'prev_vout_index', tx_inputs.prev_vout_index,
             'address', addresses.address,
-            'address_id', tx_inputs.address_id
+            'amount', tx_outputs.value::text
           )
         ) as inputs
-      `));
+      `))
+      .groupBy('tx_inputs.tx_id');
 
     const rows = await this.knex
       .with('subquery', subquery)
+      .with('agg_outputs', outputsCTE)
+      .with('agg_inputs', inputsCTE)
       .select(this.knex.raw('max_height - block_height + 1 AS confirmations'))
       .select(
         this.knex('blocks')
@@ -215,18 +233,10 @@ export default class TransactionsDAO {
         'subquery.hash', 'type', 'block_height',
         'blocks.timestamp as timestamp', 'chain_locked',
         'blocks.hash as block_hash', 'instant_lock',
-        'inputs.inputs', 'outputs.outputs'
+        'agg_inputs.inputs', 'agg_outputs.outputs'
       )
-      .leftJoin(this.knex.raw(`
-        LATERAL (
-          ${outputsSubquery.toQuery()}
-        ) outputs ON true
-      `))
-      .leftJoin(this.knex.raw(`
-        LATERAL (
-          ${inputsSubquery.toQuery()}
-        ) inputs ON true
-      `))
+      .leftJoin('agg_outputs', 'agg_outputs.tx_id', 'subquery.id')
+      .leftJoin('agg_inputs', 'agg_inputs.tx_id', 'subquery.id')
       .join(blockMaxHeightSubquery, this.knex.raw('true'))
       .leftJoin('blocks', 'blocks.height', 'block_height')
       .from('subquery')
@@ -256,42 +266,44 @@ export default class TransactionsDAO {
       .limit(limit)
       .offset(fromRank)
 
-    const outputsSubquery = this.knex('tx_outputs')
-      .whereRaw('tx_outputs.tx_id = subquery.id')
-      .select(this.knex.raw('json_agg(tx_outputs.*) as outputs'));
+    const outputsCTE = this.knex('tx_outputs')
+      .select('tx_id')
+      .select(this.knex.raw('json_agg(tx_outputs.*) as outputs'))
+      .whereIn('tx_id', this.knex('subquery').select('id'))
+      .groupBy('tx_id');
 
-    const inputsSubquery = this.knex('tx_inputs')
+    const inputsCTE = this.knex('tx_inputs')
       .leftJoin('addresses', 'addresses.id', 'tx_inputs.address_id')
-      .whereRaw('tx_inputs.tx_id = subquery.id')
+      .leftJoin('tx_outputs', function () {
+        this.on('tx_outputs.tx_id', '=', 'tx_inputs.prev_tx_id')
+          .andOn('tx_outputs.vout_index', '=', 'tx_inputs.prev_vout_index');
+      })
+      .whereIn('tx_inputs.tx_id', this.knex('subquery').select('id'))
+      .select('tx_inputs.tx_id')
       .select(this.knex.raw(`
         json_agg(
           json_build_object(
             'prev_tx_hash', tx_inputs.prev_tx_hash,
             'prev_vout_index', tx_inputs.prev_vout_index,
             'address', addresses.address,
-            'address_id', tx_inputs.address_id
+            'amount', tx_outputs.value::text
           )
         ) as inputs
-      `));
+      `))
+      .groupBy('tx_inputs.tx_id');
 
     const rows = await this.knex
       .with('subquery', subquery)
+      .with('agg_outputs', outputsCTE)
+      .with('agg_inputs', inputsCTE)
       .with('total_count', countSubquery)
+      .select(this.knex('total_count').as('total_count'))
       .select(
         'subquery.hash', 'type', 'chain_locked', 'instant_lock',
-        'inputs.inputs', 'outputs.outputs'
+        'agg_inputs.inputs', 'agg_outputs.outputs'
       )
-      .select(this.knex('total_count').as('total_count'))
-      .leftJoin(this.knex.raw(`
-        LATERAL (
-          ${outputsSubquery.toQuery()}
-        ) outputs ON true
-      `))
-      .leftJoin(this.knex.raw(`
-        LATERAL (
-          ${inputsSubquery.toQuery()}
-        ) inputs ON true
-      `))
+      .leftJoin('agg_outputs', 'agg_outputs.tx_id', 'subquery.id')
+      .leftJoin('agg_inputs', 'agg_inputs.tx_id', 'subquery.id')
       .from('subquery')
 
     const [row] = rows;
