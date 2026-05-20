4d945a10599fcb6ae39ba0066253a2cabcedcc04
owl352 <kokosinca123@gmail.com>
2026-05-20 14:51:06 +0300

    update indexes

--- indexer/migrations/V22__add_masternodes_indexes.sql
+++ indexer/migrations/V22__add_masternodes_indexes.sql
@@ -1,3 +1,5 @@
 CREATE INDEX idx_masternode_type on masternodes(type);
 CREATE INDEX idx_masternode_status on masternodes(status);
 CREATE INDEX idx_masternode_type_status ON masternodes (type, status);
+CREATE INDEX idx_masternode_pos ON masternodes(pos_penalty_score);
+CREATE INDEX idx_masternode_last_paid_time ON masternodes(last_paid_time);
